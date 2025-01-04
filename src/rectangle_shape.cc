42de3649c65c8522afe3e9dfb78297d2c2ab0b55
bramtechs <bramtech@telenet.be>
2025-01-04 15:36:39 +0100

    Rectangle shape

--- src/rectangle_shape.cc
+++ src/rectangle_shape.cc
@@ -4,8 +4,27 @@
 
 namespace demo {
 
+RectangleShape::RectangleShape()
+    : mX(0)
+    , mY(0)
+    , mWidth(0)
+    , mHeight(0)
+    , mColor(RED)
+{
+}
+
+RectangleShape::RectangleShape(int x, int y, int width, int height, Color color)
+    : mX(x)
+    , mY(y)
+    , mWidth(width)
+    , mHeight(height)
+    , mColor(color)
+{
+}
+
 void RectangleShape::draw()
 {
+    DrawRectangle(mX, mY, mWidth, mHeight, mColor);
 }
 
 }
--- src/rectangle_shape.hh
+++ src/rectangle_shape.hh
@@ -1,11 +1,31 @@
 #pragma once
 #include "shape.hh"
+#include "utils.hh"
+
+#include <raylib.h>
 
 namespace demo {
 
 class RectangleShape final : public IShape {
 public:
+    RectangleShape();
+    RectangleShape(int x, int y, int width, int height, Color color = RED);
+
     void draw() override;
+
+private:
+    int mX;
+    int mY;
+    int mWidth;
+    int mHeight;
+    Color mColor;
+
+public:
+    GETTER_SETTER(X)
+    GETTER_SETTER(Y)
+    GETTER_SETTER(Width)
+    GETTER_SETTER(Height)
+    GETTER_SETTER(Color)
 };
 
 }
